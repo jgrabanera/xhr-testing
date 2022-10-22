55ba5e9a8dfdcde087d8e9c824c195b5b1ad0dc8
jgrabanera <28018937+jgrabanera@users.noreply.github.com>
2022-10-23 01:14:12 +0800

    add individual test for PUT request intercetp in XHR Testing

--- cypress/e2e/get-request.js
+++ cypress/e2e/get-request.js
@@ -1,6 +1,10 @@
 /// <reference types="cypress" />
 
+//https://example.cypress.io/commands/network-requests
+
 describe('Network Request', () => {
+  let message = 'Unable to find comment!';
+
   beforeEach(() => {
     cy.visit('https://example.cypress.io/commands/network-requests');
   });
@@ -29,10 +33,37 @@ describe('Network Request', () => {
   });
 
   it('POST Request', () => {
-    cy.intercept('POST', '/comments').as('postComment');
+    cy.intercept('POST', '**/comments').as('postComment');
 
     cy.get('.network-post').click();
 
-    cy.wait('@postComment').its('response.statusCode').should('eq', 201); //assertion
+    //cy.wait('@postComment').its('response.statusCode').should('eq', 201); //assertion status code
+
+    cy.wait('@postComment').should(({ request, response }) => {
+      expect(request.body).to.include('email');
+      expect(response.body).to.have.property('email', 'hello@cypress.io');
+
+      expect(request.headers).to.have.property('content-type');
+      expect(response.headers).to.have.property('content-type', 'application/json; charset=utf-8');
+    });
+  });
+
+  it.only('PUT Request', () => {
+    cy.intercept(
+      {
+        method: 'PUT',
+        url: '**/comments/*', //find all url that contains /comments/
+      },
+      {
+        statusCode: 404,
+        body: { error: message },
+        delay: 500,
+      }
+    ).as('putComment');
+
+    cy.get('.network-put').click();
+
+    cy.wait('@putComment').its('response.statusCode').should('eq', 404); //assertion
+    cy.get('.network-put-comment').should('contain', message);
   });
 });
